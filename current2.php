f0ad78817ef06b47964e6216778a60d658b02e8d
shivksh <49638585+shivksh@users.noreply.github.com>
2019-04-17 09:48:13 +0530

    Update current2.php

--- current2.php
+++ current2.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>Lovely Shelter</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
